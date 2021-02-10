b9f2543270f509089612400b2ae5de2f4283cc89
willwearing <willwearing@gmail.com>
2021-02-09 18:27:25 -0700

    scaffolding of components and dependices installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//import components
+import InputForm from "./components/InputForm.tsx";
+import Console from "./components/Console.tsx";
+import Board from "./components/Board.tsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Will is the best</p>
+      <InputForm />
+      <Console />
+      <Board />
     </div>
   );
 }
