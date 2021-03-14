b8043058f58db3e3c178ca7d3d1e37f76e6f142b
willwearing <willwearing@gmail.com>
2021-03-14 11:33:27 -0600

    fixed the bug where the robot animation was persisting on all squares visited - not happy with the fix but it's working

--- src/utils/displayRobot.tsx
+++ src/utils/displayRobot.tsx
@@ -7,12 +7,26 @@
 import { RobotData } from "../constants/constants";
 
 function displayRobot(robotData: RobotData, oldLocation: string): string {
+  //
   const location = String(robotData.x) + String(robotData.y);
-  console.log(oldLocation, "Pablo");
+
   let robotPlace: any = document.getElementById(location);
+
   robotPlace.style.backgroundImage = "url('https://svgur.com/i/Tqb.svg')";
   robotPlace.style.backgroundSize = "cover";
   robotPlace.style.zIndex = 2;
+  //created this variable because of weird typescript if/else check errors.
+  //when the robot is first placed, checker == NaN, therefore, lastPlace won't
+  //need to exist, once 'MOVE' command has been entered, checker will have a value,
+  //therefore lastPlace will exist, therefore we can set the background value to null
+  let checker = parseInt(oldLocation);
+
+  if (checker) {
+    console.log("robotPlace", oldLocation);
+    let lastPlace: any = document.getElementById(oldLocation);
+    lastPlace.style.background = null;
+  }
+
   return location;
 }
 
