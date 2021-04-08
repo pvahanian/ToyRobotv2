4be82f13220e5245c01ba66ab74ac9f4c446c41c
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-04-08 14:45:53 -0700

    Removed robot ghost

--- src/utils/displayRobot.tsx
+++ src/utils/displayRobot.tsx
@@ -9,9 +9,21 @@ import { RobotData } from "../constants/constants";
 function displayRobot(robotData: RobotData, oldLocation: string): string {
   //
   const location = String(robotData.x) + String(robotData.y);
+  let checker = parseInt(oldLocation);
+    
+  if (oldLocation!="nullnull") {
+    console.log("robotPlace", oldLocation);
+    let lastPlace: any = document.getElementById(oldLocation);
+    lastPlace.style.background = null;
+  }
 
   let robotPlace: any = document.getElementById(location);
-
+  // if(oldLocation!="nullnull"){
+  //     console.log(oldLocation)
+  //   }
+  // else{
+  //         console.log(parseInt(oldLocation))
+  // }
   robotPlace.style.backgroundImage = "url('https://svgur.com/i/Tqb.svg')";
   robotPlace.style.backgroundSize = "cover";
   robotPlace.style.zIndex = 2;
@@ -19,13 +31,7 @@ function displayRobot(robotData: RobotData, oldLocation: string): string {
   //when the robot is first placed, checker == NaN, therefore, lastPlace won't
   //need to exist, once 'MOVE' command has been entered, checker will have a value,
   //therefore lastPlace will exist, therefore we can set the background value to null
-  let checker = parseInt(oldLocation);
 
-  if (checker) {
-    console.log("robotPlace", oldLocation);
-    let lastPlace: any = document.getElementById(oldLocation);
-    lastPlace.style.background = null;
-  }
 
   return location;
 }
