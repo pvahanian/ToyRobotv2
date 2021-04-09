57bd54a5a292c133f00047468b4c6461d67b9d14
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-04-09 10:42:12 -0700

    Removed  warnings in DisplayRobot

--- src/utils/displayRobot.tsx
+++ src/utils/displayRobot.tsx
@@ -9,10 +9,8 @@ import { RobotData } from "../constants/constants";
 function displayRobot(robotData: RobotData, oldLocation: string): string {
   //
   const location = String(robotData.x) + String(robotData.y);
-  let checker = parseInt(oldLocation);
     
-  if (oldLocation!="nullnull") {
-    // console.log("robotPlace", oldLocation);
+  if (oldLocation !== "nullnull") {
     let lastPlace: any = document.getElementById(oldLocation);
     lastPlace.style.background = null;
   }
@@ -22,11 +20,6 @@ function displayRobot(robotData: RobotData, oldLocation: string): string {
   robotPlace.style.backgroundImage = "url('https://svgur.com/i/Tqb.svg')";
   robotPlace.style.backgroundSize = "cover";
   robotPlace.style.zIndex = 2;
-  //created this variable because of weird typescript if/else check errors.
-  //when the robot is first placed, checker == NaN, therefore, lastPlace won't
-  //need to exist, once 'MOVE' command has been entered, checker will have a value,
-  //therefore lastPlace will exist, therefore we can set the background value to null
-
 
   return location;
 }
