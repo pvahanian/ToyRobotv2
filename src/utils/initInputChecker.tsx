d18b2bfab2a57b1fe03649cd31a19f9a6f248994
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-04-12 12:09:53 -0700

    Fixed Turn out of bounds error

--- src/utils/initInputChecker.tsx
+++ src/utils/initInputChecker.tsx
@@ -11,10 +11,13 @@ import turnRight from "./turnRight";
 
 import { RobotData } from "../constants/constants";
 
-function initInputChecker(robotData: RobotData, oldLocation:string): RobotData {
+function initInputChecker(
+  robotData: RobotData,
+  oldLocation: string
+): RobotData {
   switch (robotData.command) {
     case "PLACE":
-      return placeOnBoard(robotData,oldLocation);
+      return placeOnBoard(robotData, oldLocation);
     case "MOVE":
       return moveOnBoard(robotData);
     case "REPORT":
@@ -23,8 +26,16 @@ function initInputChecker(robotData: RobotData, oldLocation:string): RobotData {
       }
       return robotData;
     case "RIGHT":
+      if (robotData.x === null || robotData.y === null) {
+        robotData.error = "Robot not on the board - enter valid PLACE command";
+        return robotData;
+      }
       return turnRight(robotData);
     case "LEFT":
+      if (robotData.x === null || robotData.y === null) {
+        robotData.error = "Robot not on the board - enter valid PLACE command";
+        return robotData;
+      }
       return turnLeft(robotData);
     default:
       robotData.error = "Enter a valid input command";
