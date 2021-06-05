d2d6bfb8d3645f0f070b024b31b364ac3f01109a
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-06-05 12:58:01 -0500

    Added Arrow keys view

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -7,21 +7,25 @@
 import React, { useState, FormEvent } from "react";
 import Button from "@material-ui/core/Button";
 import Board from "./Board";
+import KeyBoard from './KeyBoard'
 
 //import utils
 import splitInputString from "../utils/splitInputString";
 import initInputChecker from "../utils/initInputChecker";
+import TransitionsModal from '../utils/robotPicker'
 
 //import interface
 import { RobotData } from "../constants/constants";
 
 //Delete later
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import turnLeft from '../utils/turnLeft'
 import turnRight from '../utils/turnRight'
 import moveOnBoard from '../utils/moveOnBoard'
 
+
 const InputForm: React.FC = () => {
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
   const [robotStart, setRobotStart] =useState(true)
   const [robotData, setRobotData] = useState<RobotData>({
     command: "",
@@ -32,30 +36,26 @@ const InputForm: React.FC = () => {
   });
   const [oldLocation, setOldLocation] = useState("");
 
-  /**
-   * Function: handleSubmit
-   * Goal:    onClick, set InputArrayOfStrings state and setInput back to ""
-   */
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    setOldLocation(String(robotData.x) + String(robotData.y));
-    console.log(input)
-    setRobotData(
-      initInputChecker(splitInputString(input, robotData), oldLocation)
-    );
-    setInput("");
-  };
+  // const handleSubmit = (e: FormEvent) => {
+  //   e.preventDefault();
+  //   setOldLocation(String(robotData.x) + String(robotData.y));
+  //   console.log(input)
+  //   setRobotData(
+  //     initInputChecker(splitInputString(input, robotData), oldLocation)
+  //   );
+  //   setInput("");
+  // };
 
   /**
    * Function: HandleDefault
    * Goal:    this will move to be the default login for the place on board and movement.  Once robot is placed on board the button will be disabled to not reset robot
-
-
-
    */
+   
     const handleDefault = () => {
+
     if(robotStart){
+    // TransitionsModal()
     setOldLocation(String(robotData.x) + String(robotData.y));
     setRobotData(initInputChecker(splitInputString("Place, 0,0, north", robotData),oldLocation));
     
@@ -68,9 +68,13 @@ const InputForm: React.FC = () => {
             ...robotData,
             facing:newFacing.facing,
           })
+          let highlight = document.getElementById("left")!;
+          highlight.classList.add('highlight');
+          setTimeout(function(){
+              highlight.classList.remove('highlight');
+          }, 75)
         }
         runthis()
-        setInput("")
       }
         if(event.keyCode===38){
         setOldLocation(String(robotData.x) + String(robotData.y));
@@ -81,9 +85,13 @@ const InputForm: React.FC = () => {
             x:newSpot.x,
             y:newSpot.y,
           })
+           let highlight = document.getElementById("top")!;
+          highlight.classList.add('highlight');
+          setTimeout(function(){
+              highlight.classList.remove('highlight');
+          }, 75)
         }
         runthis()
-        setInput("")
       }
        if(event.keyCode===39){
         setOldLocation(String(robotData.x) + String(robotData.y));
@@ -93,43 +101,26 @@ const InputForm: React.FC = () => {
             ...robotData,
             facing:newFacing.facing,
           })
+           let highlight = document.getElementById("right")!;
+          highlight.classList.add('highlight');
+          setTimeout(function(){
+              highlight.classList.remove('highlight');
+          }, 75)
         }
         runthis()
-        setInput("")
       }
     })
     setRobotStart(false)
+
     }
-  };
-
-  /**
-   * Function: handleChange
-   * Goal:    set Input state to user input
-   */
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
   };
 
   return (
     <div className="displays">
       <div className="displaySpacer">
         <br></br>
-        {/* <form > onSubmit={handleSubmit} 
-          <input
-            onChange={handleChange}
-            className="inputBox"
-            name="robotCommands"
-            type="text"
-            value={input}
-            placeholder="Input robot commands!"
-          />
-          <Button variant="contained" color="primary" className="goButton" onClick={handleSubmit}>
-            Go
-          </Button>
-        </form> 
-        */}
         <br />
+        <div></div>
         <h3>Place Robot on the Board</h3>
         <div className="arrow bounce"></div>
         <Button
@@ -138,13 +129,14 @@ const InputForm: React.FC = () => {
           className="quickStart"
           onClick={handleDefault}
         >
-          Quick Start
+          Start
         </Button>
       </div>
       <div id="showInfo">
         <div id="board"></div>
         <Board robotData={robotData} oldLocation={oldLocation} />
       </div>
+      <KeyBoard />
     </div>
   );
 };
---
+++ src/components/KeyBoard.tsx
@@ -0,0 +1,19 @@
+/**
+ * Function: keyBoard
+ * Goal:    
+ * Return:  
+ */
+
+function KeyBoard() {
+  
+  return(
+      <div id="arrowHolder">
+       <h1 id="left"  className="arrows">{"<"}</h1>
+       <h1 id="top"   className="arrows">{"^"}</h1>
+       <h1 id="right" className="arrows">{">"}</h1>
+      </div>
+  )
+  
+}
+
+export default KeyBoard;
